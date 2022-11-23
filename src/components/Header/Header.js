cecdf394939ceca482cb3a1fe2d427ba5da89fbd
neschkonesic <114104457+neschkonesic@users.noreply.github.com>
2022-11-23 19:29:14 +0100

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -16,7 +16,7 @@ const Header = () => {
     <>
       <HeaderStyles className={scroll ? "nav__scrolled" : null}>
         <h1>
-          <Link to="/">Koop</Link>
+          <Link to="/">Neško</Link>
         </h1>
         <Nav />
       </HeaderStyles>
