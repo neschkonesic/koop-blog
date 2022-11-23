0553626a58c15995df43e16be489f609d92df033
neschkonesic <114104457+neschkonesic@users.noreply.github.com>
2022-11-23 19:27:41 +0100

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 import Seo from "../components/Seo"
 import Footer from "./Footer/Footer"
 import { motion } from "framer-motion"
-import CookieConsent from "react-cookie-consent"
+//import CookieConsent from "react-cookie-consent"
 import Header from "./Header/Header"
 import Burger from "./Burger/Burger"
 import SideMenu from "./SideMenu/SideMenu"
@@ -28,31 +28,31 @@ const Layout = ({ children }) => {
         <SideMenu open={open} setOpen={setOpen} />
         <main style={{ minHeight: "100vh" }}>{children}</main>
         <Footer />
-        <CookieConsent
-          location="bottom"
-          buttonText="OK"
-          cookieName="koopBlogCookie"
-          expires={150}
-          style={{
-            background: "#222222",
-            padding: "15px 25px",
-            fontSize: "16px",
-          }}
-          buttonStyle={{
-            backgroundColor: "#fff",
-            color: "#222222",
-            fontSize: "16px",
-            fontWeight: 700,
-            padding: "8px 18px",
-          }}
-        >
-          This website uses cookies to help improve your experience. By using
-          this site you agree to the website{" "}
-          <Link to="/privacy" style={{ color: "#fff" }}>
-            privacy statement
-          </Link>
-          .
-        </CookieConsent>
+       // <CookieConsent
+         // location="bottom"
+       //   buttonText="OK"
+    //      cookieName="koopBlogCookie"
+      //    expires={150}
+       //   style={{
+        //    background: "#222222",
+        //    padding: "15px 25px",
+        //    fontSize: "16px",
+       //   }}
+       //   buttonStyle={{
+        //    backgroundColor: "#fff",
+       //     color: "#222222",
+       ////     fontSize: "16px",
+      //      fontWeight: 700,
+         //   padding: "8px 18px",
+      //    }}
+      //  >
+      //    This website uses cookies to help improve your experience. By using
+     //     this site you agree to the website{" "}
+     //     <Link to="/privacy" style={{ color: "#fff" }}>
+     //       privacy statement
+     //     </Link>
+        //  .
+      //  </CookieConsent>
       </motion.div>
     </>
   )
